64e84ec0a5f05bef1326316eef8af76d7e5d3f73
Tolesa11fikadu <tolee4530@gmail.com>
2022-09-10 16:06:16 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
-
 #include <stdio.h>
 
 /**
 * main - print in the string in the put function
-*
 *
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
