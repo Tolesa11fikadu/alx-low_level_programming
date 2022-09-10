5cbc79feae33189f523b9c233fff581bf3cb6157
Tolesa11fikadu <tolee4530@gmail.com>
2022-09-10 11:33:22 -0700

    Hello, puts
    mandatory

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-# include <stdio.h>
+#include <stdio.h>
+
 /**
- * main -print the string in the put function
- * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle
- * Return:0
- */
-int main (void)
+* main -print the string in the put function 
+* Description: using the main function 
+* this program prints "programming is like building a multilingual puzzle"
+* Return: 0
+*/
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle;
-			return (0);
-i}
+        put s("\"Programming is like building a multilingual puzzle");
+         return (0);
+}
