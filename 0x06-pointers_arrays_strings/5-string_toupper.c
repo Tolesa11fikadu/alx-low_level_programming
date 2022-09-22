3fbd2b42157b9d14964c000b2d7a0e822856fa66
Tolesa11fikadu <tolee4530@gmail.com>
2022-09-22 07:25:02 -0700

    changes all lowercase letters of a string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,7 +4,7 @@
  * @str: The string to be changed.
  * Retrun: A pointer to the changed string.
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 int i = 0;
 while (str[i] != '\0');
