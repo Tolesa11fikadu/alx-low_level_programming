600101823abd88ceac2f065ddbe7627a4c916774
Tolesa11fikadu <tolee4530@gmail.com>
2022-09-28 13:34:17 -0700

    tasks 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,19 +13,19 @@ return (1 + _lenght(s + 1));
 /**
  * checkp - ckecks if the string is palindrome
  * @i: is the index
- * @1g: is the lenght of string
+ * @lg: is the lenght of string
  * @s: is the string
  * Return:1 if it is polidrome or 0 if not
  */
-int checkp(int i, int 1g, char *s)
+int checkp(int i, int lg, char *s)
 {
-if (1g > 0)
+if (lg > 0)
 {
-if (s[i] == s[1g])
+if (s[i] == s[lg])
 {
-return (checkp(i + 1, 1g - 1, s));
+return (checkp(i + 1, lg - 1, s));
 }
-else if (s[i] != s[1g])
+else if (s[i] != s[lg])
 {
 return (0);
 }
@@ -42,4 +42,6 @@ return (1);
  * Return: return 1 if the string is a polidrome or 0 otherwise
  */
 int is_palindrome(char *s)
+{
 return (checkp(0, _lenght(s) - 1, s))
+}
