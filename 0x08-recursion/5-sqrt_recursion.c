48dc22111627e993172e7f33296a7052669654a2
Tolesa11fikadu <tolee4530@gmail.com>
2022-09-28 11:43:22 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+/**
+ * find_sqrt - find the natural  square root of inputted number
+ * @num: the number to find the square root 
+ * @root: the root to be tested
+ * Return:the square root
+ */
+int find_sqrt(int num, int root)
+{
+if ((root * root) == num)
+return (root);
+}
