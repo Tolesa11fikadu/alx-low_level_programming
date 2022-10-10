f306e190077776653d0001c3c566dffce207bbcc
Tolesa11fikadu <tolee4530@gmail.com>
2022-10-10 13:00:58 -0700

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef ABSO
-#define ABSO
-#define ABS(X) (((X) < (0)) ? (-(x)) : (x))
-#endif /* ABS(X) */
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+#endif
