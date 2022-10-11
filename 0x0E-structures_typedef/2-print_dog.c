a3cd05e22c4a1c125052c7d7958489c345c6dcc1
Tolesa11fikadu <tolee4530@gmail.com>
2022-10-10 23:13:31 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
-#include "dog.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include "dog.h"
 /**
  * print_dog - prints a dog
  * @d: dog to print
@@ -9,80 +10,17 @@ void print_dog(struct dog *d)
 {
 if (d == 0)
 return;
+}
+else
+{
 if (d->name == 0)
 printf("Name: (nil)\n")
 else
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
-if (d->owner == 0)
+if (d->owner == NULL)
 printf("Owner: (nil)\n");
 else
-printf("Owner: %s\n", d->owner);
+printf("Owner: %s\n", d->Owner);
 }
-#include <stdlib.h>
-#include "dog.h"
-/**
- * _copy - Make a copy of passed in argument
- * @src: Data to make copy of
- * Return: Pointer
- */
-char *_copy(char *src)
-{
-char *ptr;
-int i, len;
-if (src == NULL)
-{
-return (NULL);
-}
-for (len = 0; src[len] != '\0'; len++)
-;
-ptr = malloc(sizeof(char) * (len + 1));
-if (ptr == NULL)
-{
-return (NULL);
-}
-for (i = 0; src[i] != '\0'; i++)
-{
-ptr[i] = src[i];
-}
-ptr[i] = '\0';
-return (ptr);
-}
-/**
- * new_dog - Create a new dog variable
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
- * Return: Pointer to new dog variable
- */
-dog_t *new_dog(char *name, float age, char *owner)
-{
-dog_t *snoopie;
-char *new_name, *new_owner;
-if (name == NULL || owner == NULL)
-{
-return (NULL);
-}
-snoopie = malloc(sizeof(dog_t));
-if (snoopie == NULL)
-{
-return (NULL);
-}
-new_name = _copy(name);
-if (new_name == NULL)
-{
-free(snoopie);
-return (NULL);
-}
-(*snoopie).name = new_name;
-(*snoopie).age = age;
-new_owner = _copy(owner);
-if (new_owner == NULL)
-{	
-free((*snoopie).name);
-free(snoopie);
-return (NULL);
-}
-(*snoopie).owner = new_owner;
-return (snoopie);
 }
