34aeec49b49fcee7dffa4cd0b4301f7cea0a83fe
Tolesa11fikadu <tolee4530@gmail.com>
2022-10-13 12:53:53 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,11 +1,11 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
 /**
  * print_numbers - prints numbers followed by a new line
  * @separator - string to be printed
  * @n: number of integers passed to the function
- * Return: void
+ * @...: a variable numbers to be printed
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
