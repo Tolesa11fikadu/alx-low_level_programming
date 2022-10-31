f3b5513edf29f0565198603f192484eedcaabb63
Tolesa11fikadu <tolee4530@gmail.com>
2022-10-31 11:46:03 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,11 +8,11 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t o, r,  w;
+ssize_t o, r, w;
 char *buffer;
 if (filename == NULL)
 return (0);
-buffer = malloc(sizeof(char) * letters);;
+buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
 return (0);
 o = open(filename, o_RDONLY);
