2dbd8d57f22baa3138b4309e7d2dacf98c6750a9
Tolesa11fikadu <tolee4530@gmail.com>
2022-11-17 01:29:33 -0800

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -50,14 +50,21 @@ buffer = create_buffer(argv[2]);
 from = open(argv[1], O_RDONLY);
 r = read(from, buffer, 1024);
 to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-do {
+do
+{
 if (from == -1 || r == -1)							{
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);		free(buffer)									exit(98);
-}										w = write(to, buffer, r);
-if (to == -1 || w == -1)							{										dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-free(buffer);									exit(99);									}
+}
+w = write(to, buffer, r);
+if (to == -1 || w == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+free(buffer);
+exit(99);
+}
 r = read(from, buffer, 1024);
-to = open(argv[2], O_WRONLY | O_APPEND);					}while (r > 0);
+to = open(argv[2], O_WRONLY | O_APPEND);
+} while (r > 0);
 free(buffer);
 close_file(from);
 close_file(to);									return (0);
